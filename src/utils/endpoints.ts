d3598c5d5c2a5c17fe394755c7353464f255dc37
JLW157 <alexbiletskyi17@gmail.com>
2023-03-10 09:38:16 +0200

    changes in enpoints.ts

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,4 +1,4 @@
-const baseUrl = "https://urlshortenerapi1.azurewebsites.net/api";
+const baseUrl = "https://localhost:7107/api";
 
 export const urlShortUrl = `${baseUrl}/shorturl`;
 export const urlAccounts = `${baseUrl}/accounts`;
