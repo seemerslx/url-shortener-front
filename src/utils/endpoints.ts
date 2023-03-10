0193bd56b64153793322d51765c6e0c87213267b
JLW157 <alexbiletskyi17@gmail.com>
2023-03-10 09:00:55 +0200

    setup gh-pages

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,4 +1,4 @@
-const baseUrl = "https://localhost:7107/api";
+const baseUrl = "https://urlshortenerapi1.azurewebsites.net/api";
 
 export const urlShortUrl = `${baseUrl}/shorturl`;
 export const urlAccounts = `${baseUrl}/accounts`;
