2205787514bf3482dabe79983b34a8cb13f1cd3c
JLW157 <alexbiletskyi17@gmail.com>
2023-03-15 15:09:46 +0200

    test

--- src/components/UI/Home.tsx
+++ src/components/UI/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return <>
         <div className="m-3 text-center">
-            <h2>Hello this home page of my url shortener application</h2>
+            <h2>Hello this home page of my url shortener application (test)</h2>
         </div>
     </>
 };
